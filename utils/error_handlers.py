e76d3f7736a7582d74ad62f3e4808d3a595afbc5
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2026-04-04 13:05:46 -0600

    Bug en nombre de funciones

--- utils/error_handlers.py
+++ utils/error_handlers.py
@@ -7,17 +7,17 @@ def register_error_handlers(app):
         return jsonify({'error': 'Bad Request', 'mensaje': str(e)}), 400
 
     @app.errorhandler(401)
-    def bad_request(e):
-        return jsonify({'error': 'Bad Request', 'mensaje': str(e)}), 401
+    def unauthorized(e):
+        return jsonify({'error': 'Unauthorized', 'mensaje': str(e)}), 401
 
     @app.errorhandler(403)
-    def bad_request(e):
-        return jsonify({'error': 'Bad Request', 'mensaje': str(e)}), 403
+    def forbidden(e):
+        return jsonify({'error': 'Forbidden', 'mensaje': str(e)}), 403
 
     @app.errorhandler(404)
-    def bad_request(e):
-        return jsonify({'error': 'Bad Request', 'mensaje': str(e)}), 404
+    def not_found(e):
+        return jsonify({'error': 'Not Found', 'mensaje': str(e)}), 404
 
-    @app.errorhandler(404)
-    def bad_request(e):
-        return jsonify({'error': 'Bad Request', 'mensaje': str(e)}), 404
+    @app.errorhandler(500)
+    def server_error(e):
+        return jsonify({'error': 'Internal Server Error', 'mensaje': str(e)}), 500
